e3cff85102bb6bfeefe0fcfd34173b14f4ff81bf
thegreatpeon <thegreatpeon@gmail.com>
2023-02-10 08:15:11 -0800

    Updated to show list of heroes. Villains is a placeholder.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+//export class HeroesComponent {
+//  hero: Hero = {
+//    id: 1,
+//    name: 'Windstorm'
+//  };
+//}
+
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  onClear(): void {
+    this.selectedHero = undefined;
+  }
+}
---
+++ src/app/villains/villains.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-villains',
+  templateUrl: './villains.component.html',
+  styleUrls: ['./villains.component.css']
+})
+export class VillainsComponent {
+
+}
