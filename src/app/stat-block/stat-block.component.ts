82702100f61653a3760bb9c9d9ec3dfcd8605043
thegreatpeon <thegreatpeon@gmail.com>
2023-02-15 15:12:47 -0800

    Thinking about making the heros more complicated.

---
+++ src/app/stat-block/stat-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-block',
+  templateUrl: './stat-block.component.html',
+  styleUrls: ['./stat-block.component.css']
+})
+export class StatBlockComponent {
+
+}
